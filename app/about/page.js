9e935ca508101687d5ae971db670e188e99694f3
Nalla Manoj Kumar <100987446+manojnalla098@users.noreply.github.com>
2023-09-20 12:40:10 +0530

    11

--- app/about/page.js
+++ app/about/page.js
@@ -1,47 +1,4 @@
-const title = "About Page Title";
-const description = "About Page description";
-const url = "https://alikidi.com";
-const image =
-  "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=1024x1024&w=is&k=20&c=-AdIwLn-nIYlgctc95CZuJHnkku_ia-f8A8m2LFwl2A=";
-
-export function generateMetadata() {
-  return {
-    title: "About Page",
-    description: "About Page description",
-    openGraph: {
-      title: title,
-      description: description,
-      url: url,
-      image: "",
-      siteName: "Alikidi.com",
-      images: [
-        {
-          url: image,
-          width: 800,
-          height: 600,
-        },
-        {
-          url: image,
-          width: 1800,
-          height: 1600,
-          alt: "Legato Designs",
-        },
-      ],
-      type: "website",
-    },
-    twitter: {
-      card: "alikidi",
-      title: title,
-      description: description,
-      creator: "Dexterous Texhnology",
-      images: [image],
-    },
-    alternates: {
-      canonical: "https://alikidi.com",
-    },
-    manifest: "https://nextjs.org/manifest.json",
-  };
-}
+"use client";
 const Page = () => {
   return (
     <>
